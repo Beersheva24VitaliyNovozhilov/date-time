72ebc538a9c770fd5afcf1d8c5d5318b0f7211ba
p4r53c <n1chr0mix@gmail.com>
2024-08-12 15:50:13 +0300

    HW 16 LocalDat instead ChronoLocalDate

--- src/main/java/io/p4r53c/telran/PastTemporalDateProximity.java
+++ src/main/java/io/p4r53c/telran/PastTemporalDateProximity.java
@@ -1,59 +1,50 @@
 package io.p4r53c.telran;
 
-import java.time.chrono.ChronoLocalDate;
+import java.time.LocalDate;
 import java.time.temporal.Temporal;
 import java.time.temporal.TemporalAdjuster;
 import java.util.Arrays;
 
-/**
- * Class that implements the TemporalAdjuster interface and provides a way to find the
- * closest past date from a given set of past dates.
- *
- * @author p4r53c
- */
 public class PastTemporalDateProximity implements TemporalAdjuster {
 
-    private ChronoLocalDate[] chronoLocalDates;
+    private LocalDate[] localDates;
 
-    /**
-     * Constructs a new PastTemporalDateProximity object.
-     *
-     * @param temporals the array of past Temporal objects
-     */
     public PastTemporalDateProximity(Temporal[] temporals) {
-        this.chronoLocalDates = Arrays.copyOf(new ChronoLocalDate[temporals.length], temporals.length);
+        this.localDates = new LocalDate[temporals.length];
 
         int index = 0;
         for (Temporal temporal : temporals) {
-            this.chronoLocalDates[index++] = ChronoLocalDate.from(temporal);
+            this.localDates[index++] = convertToLocalDate(temporal);
         }
-        Arrays.sort(this.chronoLocalDates);
+
+        Arrays.sort(this.localDates);
     }
 
-    /**
-     * Adjusts the given Temporal object to the closest past date from the set of past dates.
-     *
-     * @param temporal the Temporal object to be adjusted
-     * @return the adjusted Temporal object or null if no past date is found
-     */
     @Override
     public Temporal adjustInto(Temporal temporal) {
-        ChronoLocalDate targetDate = ChronoLocalDate.from(temporal);
-
+        LocalDate targetDate = convertToLocalDate(temporal);
         int low = 0;
-        int high = chronoLocalDates.length - 1;
+        int high = localDates.length - 1;
         int resultIndex = -1;
 
         while (low <= high) {
             int mid = low + (high - low) / 2;
-            if (chronoLocalDates[mid].isBefore(targetDate)) {
+            if (localDates[mid].isBefore(targetDate)) {
                 resultIndex = mid;
                 low = mid + 1;
             } else {
                 high = mid - 1;
             }
         }
+        return resultIndex != -1 ? temporal.with(localDates[resultIndex]) : null;
+    }
 
-        return resultIndex != -1 ? temporal.with(chronoLocalDates[resultIndex]) : null;
+    private LocalDate convertToLocalDate(Temporal temporal) {
+        try {
+            return LocalDate.from(temporal);
+        } catch (Exception e) {
+            throw new IllegalArgumentException(
+                    "Cannot convert temporal to LocalDate: " + temporal.getClass().getName());
+        }
     }
 }
